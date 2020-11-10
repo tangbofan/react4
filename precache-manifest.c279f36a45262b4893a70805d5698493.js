d76cc7562fb8801085f6cce96823272f7cc4fce4
唐博凡 <44659457+tangbofan@users.noreply.github.com>
2020-11-10 12:52:38 +0800

    Add files via upload

---
+++ precache-manifest.c279f36a45262b4893a70805d5698493.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb55e37cc5e0391903463afea248bca6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b828532b869c79d1654e",
+    "url": "./static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "cff02a6555c1d874b1a5",
+    "url": "./static/css/main.20a12222.chunk.css"
+  },
+  {
+    "revision": "b828532b869c79d1654e",
+    "url": "./static/js/2.19c7a57d.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "./static/js/2.19c7a57d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cff02a6555c1d874b1a5",
+    "url": "./static/js/main.179a49f6.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  }
+]);
